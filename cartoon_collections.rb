fd781a64126788becfc365a3c35b52893143f355
Eli Landau <eli.b.landau@gmail.com>
2020-06-08 16:34:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   
-  array.each_with_index do |dwarf|
-  print array[dwarf]
+  array.each_with_index do |dwarf, index|
+  print "#{index}. #{dwarf}"
   end
 end
